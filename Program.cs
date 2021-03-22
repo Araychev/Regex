cc3cc610ea770bab3608a131524a92298fa5e20e
Angel Raychev <angelraychev@gmail.com>
2021-03-22 20:38:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexlAB
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string patern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+            var regex = new Regex(patern);
+
+            MatchCollection validNames = Regex.Matches(input, patern);
+
+            foreach (Match item in validNames)
+            {
+                Console.Write($"{item.Value} ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
